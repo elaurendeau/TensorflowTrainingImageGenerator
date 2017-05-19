d94bc1a1aca65df156de160b19fa432e6244abce
Elliot <laurendeau.elliot@gmail.com>
2017-05-19 07:30:07 -0400

    print with font

--- src/app.kt
+++ src/app.kt
@@ -1,3 +1,7 @@
+import java.awt.Color
+import java.awt.Font
+import java.awt.font.FontRenderContext
+import java.awt.font.TextLayout
 import java.io.ByteArrayOutputStream
 import java.io.FileOutputStream
 import java.net.URL
@@ -6,8 +10,11 @@ import javax.imageio.ImageIO
 fun main(args: Array<String>) {
     val url: String = "https://images2.onionstatic.com/clickhole/3564/7/original/600.jpg";
     val img = ImageIO.read(URL(url))
-    val gfx = img.createGraphics();
-    gfx.drawString("test", 10, 10);
+    val gfx = img.createGraphics()
+    gfx.setFont(Font("TimesRoman", Font.PLAIN, 50))
+    gfx.setColor(Color.BLUE)
+    gfx.drawString("test", 80, 80)
+
     val byteArrayOutputStream = ByteArrayOutputStream();
     ImageIO.write(img, "png", byteArrayOutputStream);
     FileOutputStream("test.png").write(byteArrayOutputStream.toByteArray());
