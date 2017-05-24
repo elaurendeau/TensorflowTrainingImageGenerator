668d9e1d489ad682834ae6a09e0b5415f0445bd1
Elliot <laurendeau.elliot@gmail.com>
2017-05-23 23:20:03 -0400

    Migrating code from the main toward the right archi. Next step: create a usecase

--- src/main/kotlin/interface/controller/ImageController.kt
+++ src/main/kotlin/interface/controller/ImageController.kt
@@ -1,15 +1,28 @@
 package `interface`.controller
 
+import domain.poco.Position
+import domain.poco.Text
 import infrastructure.IImageHandler
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Controller
+import java.awt.Color
+import java.awt.Font
+import java.io.ByteArrayOutputStream
+import java.io.FileOutputStream
+import java.net.URL
+import javax.imageio.ImageIO
 
 @Component
 open class ImageController @Autowired constructor(val imageHandler: IImageHandler) {
 
     fun run(url: String) {
-        println("test" + imageHandler)
+
+        val img = ImageIO.read(URL(url))
+
+        val modifiedImage = imageHandler.writeTextOnImageAtSpecificPosition(img, Position(100,100), Text("Test", Font("TimesRoman", Font.PLAIN, 50), Color.black))
+        val byteArrayOutputStream = ByteArrayOutputStream();
+        ImageIO.write(modifiedImage, "png", byteArrayOutputStream);
+        FileOutputStream("test.png").write(byteArrayOutputStream.toByteArray());
     }
 
 
