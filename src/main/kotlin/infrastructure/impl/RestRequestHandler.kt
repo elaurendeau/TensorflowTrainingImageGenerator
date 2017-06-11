ab7c889d11f7e0bd26a3dfe1b8a3a0af73b30300
Elliot <laurendeau.elliot@gmail.com>
2017-06-11 10:09:55 -0400

    removed the async post + changed the return type to string. Next step is to add kafka or any queue support

--- src/main/kotlin/infrastructure/impl/RestRequestHandler.kt
+++ src/main/kotlin/infrastructure/impl/RestRequestHandler.kt
@@ -1,31 +1,21 @@
 package infrastructure.impl
 
-import domain.FileRequest
-import domain.Request
 import domain.RestRequest
 import dto.RestResponse
-import infrastructure.IFileRequestHandler
 import infrastructure.IRestRequestHandler
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.async
+import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import java.awt.image.BufferedImage
 import java.awt.image.DataBufferByte
-import java.awt.image.WritableRaster
-import java.io.ByteArrayOutputStream
-import java.io.FileOutputStream
-import javax.imageio.ImageIO
 
 @Service
 class RestRequestHandler : IRestRequestHandler {
     override fun send(request: RestRequest, image: BufferedImage, label: String) {
-        async(CommonPool) {
-            val template = RestTemplate()
-            val data: DataBufferByte = image.raster.dataBuffer as DataBufferByte
-            val response = RestResponse(data.data, label)
+        val template = RestTemplate()
+        val data: DataBufferByte = image.raster.dataBuffer as DataBufferByte
+        val response = RestResponse(data.data, label)
 
-            template.postForObject(request.callback, response, RestResponse::class.java, HashMap<String, String>())
-        }
+        template.postForObject(request.callback, response, String::class.java, HashMap<String, String>())
     }
 }
